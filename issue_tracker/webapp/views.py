e90a1488b50fab9319f1d46aa0daef62300e5fcb
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-16 05:28:14 +0600

    Added static folder with css & Added base,index templates & Added IndexView class to the views & added 'index' path to urls

--- issue_tracker/webapp/views.py
+++ issue_tracker/webapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views.generic import View, TemplateView
+from webapp.models import Type, Status, Issue
 
-# Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        issues = Issue.objects.all()
+        context = {'issues': issues}
+        return render(request, 'index.html', context)
