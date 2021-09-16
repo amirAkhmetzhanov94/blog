641e0be3655159255cd22067401f3ac306613903
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-16 15:25:27 +0600

    Rewrote the 'IndexView' class following the specs (changed 'View' inheritance to the 'TemplateView')

--- issue_tracker/webapp/views.py
+++ issue_tracker/webapp/views.py
@@ -3,8 +3,6 @@ from django.views.generic import View, TemplateView
 from webapp.models import Type, Status, Issue
 
 
-class IndexView(View):
-    def get(self, request, *args, **kwargs):
-        issues = Issue.objects.all()
-        context = {'issues': issues}
-        return render(request, 'index.html', context)
+class IndexView(TemplateView):
+    template_name = 'index.html'
+    extra_context = {'issues': Issue.objects.all()}
