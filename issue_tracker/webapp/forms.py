cce11f63e0ccc88c40be4655db715339613aba0c
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-16 17:04:49 +0600

    Added forms to the project

---
+++ issue_tracker/webapp/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+from django.forms import widgets
+from webapp.models import Type, Status
+
+special_attributes = {"class": "form-control"}
+
+
+class IssueForm(forms.Form):
+    summary = forms.CharField(max_length=2000, required=True, label='Summary',
+                              widget=widgets.Input(attrs=special_attributes))
+    description = forms.CharField(max_length=2000, label='Description',
+                                  widget=widgets.Textarea(attrs=special_attributes))
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Status",
+                                    widget=widgets.ChoiceWidget(attrs=special_attributes))
+    type = forms.ModelChoiceField(queryset=Type.objects.all(), label="Type",
+                                  widget=widgets.ChoiceWidget(attrs=special_attributes))
