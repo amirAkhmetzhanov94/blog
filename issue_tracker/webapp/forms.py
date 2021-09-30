1776d66108574f2ade8bf30533df141ed7240868
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-30 17:36:08 +0600

    HW-53 Добавил стилизацию для полей формы

--- issue_tracker/webapp/forms.py
+++ issue_tracker/webapp/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from django.forms import widgets
-from webapp.models import Issue
+from webapp.models import Issue, Status, Type
 
 special_attributes = {"class": "form-control"}
 
 
 class IssueForm(forms.ModelForm):
+    summary = forms.CharField(widget=widgets.TextInput(attrs=special_attributes))
+    description = forms.CharField(widget=widgets.Textarea(attrs=special_attributes))
+    status = forms.ModelChoiceField(queryset=Status.objects.all(), label="Status",
+                                    widget=widgets.Select(attrs=special_attributes))
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), label="Type",
+                                          widget=widgets.SelectMultiple(attrs=special_attributes))
+
     class Meta:
         exclude = ["creation_time", "update_time"]
         model = Issue
