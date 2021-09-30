e19e565aa624575734bd8f907c81402e1959873a
Akhmetzhanov_Amir <amir.akhmetzhanov94@gmail.com>
2021-09-30 17:10:41 +0600

    HW-53 Добавил в модель два типа валидаций и сделал миграцию.

--- issue_tracker/webapp/models.py
+++ issue_tracker/webapp/models.py
@@ -1,4 +1,30 @@
+from django.core.validators import BaseValidator
 from django.db import models
+from django.utils.deconstruct import deconstructible
+
+
+@deconstructible
+class MaxValueValidator(BaseValidator):
+    message = "Value %(value)s has length %(show_value)d. " \
+              "Length should be less than %(limit_value)d"
+
+    def clean(self, x):
+        return len(x)
+
+    def compare(self, a, b):
+        return a > b
+
+
+@deconstructible
+class MinValueValidator(BaseValidator):
+    message = "Value %(value)s has length %(show_value)d. " \
+              "Min Length should be more than %(limit_value)d"
+
+    def clean(self, x):
+        return len(x)
+
+    def compare(self, a, b):
+        return a < b
 
 
 class Type(models.Model):
@@ -16,8 +42,10 @@ class Status(models.Model):
 
 
 class Issue(models.Model):
-    summary = models.CharField(max_length=30, null=False, blank=False, verbose_name='Summary')
-    description = models.TextField(max_length=2000, blank=True, null=True, verbose_name='Description')
+    summary = models.CharField(max_length=30, validators=[MaxValueValidator(30)], null=False, blank=False,
+                               verbose_name='Summary')
+    description = models.TextField(max_length=2000, validators=[MinValueValidator(100)],  blank=True, null=True,
+                                   verbose_name='Description')
     status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, verbose_name='Status', related_name='issues')
     type = models.ManyToManyField('webapp.Type', related_name='issues')
     creation_time = models.DateTimeField(auto_now_add=True, verbose_name='Creation time')
