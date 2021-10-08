80d4d013bba6741020486637ef19225ccb9909a4
Amir Akhmetzhanov <amir.akhmetzhanov94@gmail.com>
2021-10-09 04:04:17 +0600

    HW-55 Добавил в модель класс 'проект' и связал класс 'задача' с классом 'проект'. А также сделал новую миграцию в соответствии с новой моделью

--- issue_tracker/webapp/admin.py
+++ issue_tracker/webapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from webapp.models import Type, Status, Issue
+from webapp.models import Type, Status, Issue, Project
 
 admin.site.register(Type)
 admin.site.register(Status)
 admin.site.register(Issue)
+admin.site.register(Project)
--- issue_tracker/webapp/models.py
+++ issue_tracker/webapp/models.py
@@ -44,12 +44,23 @@ class Status(models.Model):
 class Issue(models.Model):
     summary = models.CharField(max_length=30, validators=[MaxValueValidator(30)], null=False, blank=False,
                                verbose_name='Summary')
-    description = models.TextField(max_length=2000, validators=[MinValueValidator(100)],  blank=True, null=True,
+    description = models.TextField(max_length=2000, validators=[MinValueValidator(100)], blank=True, null=True,
                                    verbose_name='Description')
     status = models.ForeignKey('webapp.Status', on_delete=models.PROTECT, verbose_name='Status', related_name='issues')
     type = models.ManyToManyField('webapp.Type', related_name='issues')
     creation_time = models.DateTimeField(auto_now_add=True, verbose_name='Creation time')
     update_time = models.DateTimeField(auto_now=True, verbose_name='Update time')
+    task_id = models.ForeignKey('Project', on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'{self.summary} ({self.status}, {self.type.get()})'
+
+
+class Project(models.Model):
+    start_date = models.DateField()
+    finish_date = models.DateField(blank=True)
+    project_name = models.CharField(max_length=30)
+    project_summary = models.TextField()
+
+    def __str__(self):
+        return f'{self.project_name}'
