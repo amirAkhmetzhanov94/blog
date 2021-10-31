2541ff358b57b8bfd2e2984a211ec275e20f3c2c
Amir Akhmetzhanov <amir.akhmetzhanov94@gmail.com>
2021-10-31 22:44:56 +0600

    HW-60 Добавил модель профиля в приложение Accounts. Установил пакет 'pillow' для работы с изображениями и добавил новую зависимость в requirements.txt. Добавил папку для загрузки изображений под аватары, прописал пути к папке в настройках.

--- issue_tracker/accounts/models.py
+++ issue_tracker/accounts/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(
+        get_user_model(),
+        related_name="profile",
+        on_delete=models.CASCADE,
+        verbose_name="User"
+    )
+    avatar = models.ImageField(null=True, blank=True,
+                               upload_to="avatars", verbose_name="Avatar")
+    github_link = models.URLField(null=True, blank=True,
+                                  verbose_name="Github Profile")
+    about = models.TextField(null=True, blank=True,
+                             verbose_name="About")
+
+    class Meta:
+        verbose_name = "Profile"
+        verbose_name_plural = "Profiles"
